e9b847194c06238ba17763d8261034deef77c80d
jondhc <jonathandaniel.heca@gmail.com>
2017-05-14 21:23:41 -0500

    Finished project

---
+++ app/src/main/java/com/jon_dhc/to_do_first/MainActivity.java
@@ -0,0 +1,58 @@
+package com.jon_dhc.to_do_first;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        items = new ArrayList<String>();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        items.add("Shower");
+        items.add("Breakfast");
+        items.add("Exercise");
+        //setup remove listener method call
+        setupListViewListener();
+    }
+
+    public void onAddItem(View v){
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etNewItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etNewItem.setText("");
+    }
+
+    //attaches a long click listener to the lisview
+    private void setupListViewListener(){
+        lvItems.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener(){
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter, View item, int pos, long id){
+                        //Remove the item within array
+                        items.remove(pos);
+                        //Refresh the adapter
+                        itemsAdapter.notifyDataSetChanged();
+                        //Return true marks longClick event handled
+                        return true;
+                    }
+                }
+        );
+    }
+}
